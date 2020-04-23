15819cf3a3df35e807d961d16a2eade5f7050e80
dlwjddn22 <dlwjddn22@nate.com>
2020-04-23 15:57:36 +0900

    [제목] thymeleaf(타임리프) 사용법 [내용] 1. 타임리프 사용법 (templates > html추가) 2. 컨트롤러에서 html로 값보내기

---
+++ src/main/java/com/jflow/sbp/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.jflow.sbp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jflow.sbp.domain.User;
+import com.jflow.sbp.mapper.UserMapper;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserMapper mapper;
+	
+	@RequestMapping(value="/korean", method=RequestMethod.POST)
+	public String korean(@RequestParam String name, Model model) throws Exception {
+		System.out.println("name=" + name);
+		model.addAttribute("name", name);
+		return "hello2";
+	}
+	
+	@RequestMapping("/hello2")
+	public void hello2(Model model) throws Exception{
+		String uid = "user1";
+		model.addAttribute("uname", mapper.getUname(uid));
+		
+		User user = mapper.getLoginInfo(uid);
+		model.addAttribute("user", user);
+		
+		List<User> users = mapper.getUsers();
+		model.addAttribute("users", users);
+	}
+}
--- src/main/java/com/jflow/sbp/mapper/UserMapper.java
+++ src/main/java/com/jflow/sbp/mapper/UserMapper.java
@@ -1,5 +1,7 @@
 package com.jflow.sbp.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
@@ -7,7 +9,10 @@ import com.jflow.sbp.domain.User;
 
 public interface UserMapper {
 	@Select("select uname from User where uid=#{uid}")
-	public User getUname(@Param("uid") String uid) throws Exception;
+	public String getUname(@Param("uid") String uid) throws Exception;
 	
 	public User getLoginInfo(@Param("uid") String uid) throws Exception;
+	
+	@Select("select * from User limit 100")
+	public List<User> getUsers() throws Exception;
 }
