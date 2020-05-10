e5d3dd57e1338d3360cdd792041286a2e854356d
dlwjddn22 <dlwjddn22@nate.com>
2020-05-11 00:50:57 +0900

    refs #4 : bootstrap theme SB Admin 2 적용

--- src/main/java/com/jflow/sbp/controller/SbpController.java
+++ src/main/java/com/jflow/sbp/controller/SbpController.java
@@ -6,12 +6,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SbpController {
-	
+
 	@RequestMapping("/")
-	public String main(Model model) throws Exception
-	{
-		model.addAttribute("hello","Spring Boot에 오신것을 환영합니다.");
+	public String login(Model model) throws Exception {
+		model.addAttribute("hello", "Spring Boot에 오신것을 환영합니다.");
 		return "index";
+	}
+
+	@RequestMapping("/main")
+	public String main(Model model) throws Exception {
+		return "/contents/main";
+	}
+
+	@RequestMapping("/page1")
+	public String page1(Model model) throws Exception {
+		return "/contents/page1";
+	}
+
+	@RequestMapping("/page2")
+	public String page2(Model model) throws Exception {
+		return "/contents/page2";
 	}
 
 }
